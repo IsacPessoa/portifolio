01163a6e0f7e08667d8fcee34789d91f3eb738dc
Isac Pessoa <isacdavidpessoa@hotmail.com>
2023-10-20 19:53:10 -0300

    Colocando imgs e finalizando secao sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       labore error sequi provident accusamus dolores fugiat ullam laborum
       ducimus, qui autem!
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=IsacPessoa&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=IsacPessoa&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
