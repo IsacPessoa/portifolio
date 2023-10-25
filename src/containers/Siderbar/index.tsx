49448fe8a6e973fb841e15c30e2a75539afcb2fb
Isac Pessoa <isacdavidpessoa@hotmail.com>
2023-10-25 20:52:58 -0300

    estilizando a sidebar

--- src/containers/Siderbar/index.tsx
+++ src/containers/Siderbar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/IsacPessoa.png" />
-    <Title fontSize={20}>Isac Pessoa</Title>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={20}>Isac Pessoa</Title>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        IsacPessoa
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
