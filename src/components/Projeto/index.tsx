f395e82c671307070613cc078f0d7f94f5dc805d
Isac Pessoa <isacdavidpessoa@hotmail.com>
2023-10-25 11:36:39 -0300

    iniciando estrutura de card de projetos

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,11 @@
+import Paragrafo from '../Paragrafo'
+import Title from '../Title'
+
+const Projeto = () => (
+  <div>
+    <Title>Projeto Lista de tarefas</Title>
+    <Paragrafo>Lista de tarefas feita com VueJS</Paragrafo>
+  </div>
+)
+
+export default Projeto
--- src/containers/Projetos/index.tsx
+++ src/containers/Projetos/index.tsx
@@ -1,8 +1,14 @@
+import Projeto from '../../components/Projeto'
 import Title from '../../components/Title'
 
 const Projetos = () => (
   <section>
     <Title fontSize={16}>Projetos</Title>
+    <ul>
+      <li>
+        <Projeto />
+      </li>
+    </ul>
   </section>
 )
 
